f3bfd939449ddea9f9de932fc9038b4110b240dc
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-14 13:36:57 +0000

    variable

--- 01_basics/Hello_01.java
+++ 01_basics/Hello_01.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class Hello_01 {
     public static void main ( String args []) {
         System.out.println("Hello word..!");
         System.out.println("I am VK");
---
+++ 01_basics/Variable_02.java
@@ -0,0 +1,12 @@
+public class Variable_02 {
+    public static void main ( String a[]) {
+        int num1 = 7;
+        int num2 = 14;
+        int result = num1 + num2;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num1 + num2);
+        System.out.println(result);
+    }
+}
