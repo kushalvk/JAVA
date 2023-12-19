d6a2c27ee602c97df2bc26a2f85e5309fea45703
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-19 16:19:41 +0000

    Loops

---
+++ 03_Loops/Need_for_loop_01.java
@@ -0,0 +1,11 @@
+
+public class Need_for_loop_01 {
+    public static void main(String[] args) {
+        // repate this statment maltiple time
+        // loop - while loop
+        //      - do while
+        //      - for
+
+        System.out.println("Hii..! Kushal");
+    }
+}
