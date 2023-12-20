488ddfe2ef74b9fc65cf96b36347efc17c149f2d
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-20 17:11:39 +0000

    do while loop

---
+++ 03_Loops/Do_while_loop_03.java
@@ -0,0 +1,9 @@
+public class Do_while_loop_03 {
+    public static void main(String[] args) {
+        int i = 6;
+
+        do {
+            System.out.println("Kushal " + i);
+        } while (i <= 5);
+    }
+}
