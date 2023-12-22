e2ce4be73315ecf07e23c68c9e87505810a47d7f
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-22 13:55:40 +0000

    Done Projrct

--- Java_Projects/Student_Result.java
+++ Java_Projects/Student_Result.java
@@ -8,19 +8,23 @@ public class Student_Result {
         System.out.println("Enter a roll number: ");
         s.nextInt();
 
-        int Marks[] = new int[5];
+        int Marks[] = new int[6];
 
         for(int i = 1 ; i <= 5 ; i++) {
             System.out.println("Marks " + i + ": ");
-            Marks[i] = s.nextInt();
+            Marks[i] += s.nextInt();
         }
+        int Total = Marks[1] + Marks[2] + Marks[3] + Marks[4] + Marks[5];
 
-        // int Arr[] = {10,20,30,40,50};
-        // int Total = Arr[0] + Arr[1] + Arr[2] + Arr[3] + Arr[4];
+        System.out.println("Total: "+ Total);
 
-        // System.out.println("Total: "+ Total);
-
-        // int Result = Total/5;
-        // System.out.println(Result);
+        int Result = Total/5;
+        System.out.println("Your result is " + Result + " %");
+        
+        if(Result < 30) {
+            System.out.println("you are FAIL ....beter luck next time ....!");
+        } else {
+            System.out.println("You are PASS ...Congrats...!");
+        }
     }
 }
