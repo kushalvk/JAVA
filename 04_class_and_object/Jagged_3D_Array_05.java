15af1531134ed55a191a71363a2dd9ceea17c002
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-22 15:41:49 +0000

    3D and jagged array

---
+++ 04_class_and_object/Jagged_3D_Array_05.java
@@ -0,0 +1,18 @@
+public class Jagged_3D_Array_05 {
+    public static void main(String[] args) {
+        int num[][] = new int[3][4];
+        // int num[][][] = new int[3][4][5];     // 3D Array
+
+        num[0] = new int[3]; // Jagged array
+        num[1] = new int[4];
+        num[2] = new int[2];
+        
+        for(int i = 0; i < num.length; i++) {
+            for(int j = 0; j < num[i].length; j++) {
+                num[i][j] = (int) (Math.random() * 10); // for genrat a random intger number
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
