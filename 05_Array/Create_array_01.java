9d28e993915038ac0020ae099005aad4258aa528
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-22 13:55:12 +0000

    Array

---
+++ 05_Array/Create_array_01.java
@@ -0,0 +1,15 @@
+public class Create_array_01 {
+    public static void main(String[] args) {
+        int nums[] = new int[4];
+
+        nums[0] = 6; // chnage in value
+        nums[1] = 9; 
+        nums[2] = 2; 
+        nums[3] = 7; 
+
+        for(int i = 0; i <= 3; i++) {
+            System.out.println(nums[i]);
+        }
+        
+    }
+}
