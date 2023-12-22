875c648cc483b3516a735b1634b375ae73022e57
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-22 16:54:40 +0000

    String

---
+++ 06_String/String_01.java
@@ -0,0 +1,10 @@
+public class String_01 {
+    public static void main(String[] args) {
+        // String name = new String("kushal"); // hardly use
+
+        String name = "kushal";
+        System.out.println("Hello " + name);
+        // System.out.println(name.charAt(1)); // u
+        System.out.println(name.concat(" vaghela")); // kushal vaghela
+    }
+}
