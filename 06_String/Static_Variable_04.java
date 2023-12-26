da1be7be1de98281975f29f8f4068b8b69c9054b
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-26 05:01:31 +0000

    Static Variable

---
+++ 06_String/Static_Variable_04.java
@@ -0,0 +1,28 @@
+class Mobile {
+    String brand;
+    int price;
+    static String name;
+
+    public void show() {
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+}
+
+public class Static_Variable_04 {
+    public static void main(String[] args) {
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 1500;
+        obj1.name = "SmartPhone";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 1700;
+        obj2.name = "SmartPhone";
+
+        obj1.name = "Phone";
+
+        obj1.show();
+        obj2.show();
+    }
+}
