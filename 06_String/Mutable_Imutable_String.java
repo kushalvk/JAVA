ba6a46abea52da6002f0c3c140d7b6d1a2e33df8
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2023-12-26 04:27:23 +0000

    Mutable/Immutable String

---
+++ 06_String/Mutable_Imutable_String.java
@@ -0,0 +1,13 @@
+public class Mutable_Imutable_String {
+    public static void main(String[] args) {
+        String name = "kushal";
+        name = name + " Vaghela";
+        System.out.println("Hello " + name);
+
+        String s1 = "Kushal";
+        String s2 = "Kushal"; // not create other memory / same memory as s1.
+
+        // Mutable String = changeble
+        // Immutable String = unchangeble
+    }
+}
