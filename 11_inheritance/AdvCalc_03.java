c74d1e76ddb187f85e07bf2cea31c7278469c43a
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-01-30 14:02:40 +0000

    inheritance

---
+++ 11_inheritance/AdvCalc_03.java
@@ -0,0 +1,9 @@
+public class AdvCalc_03 extends Calc_02 {
+    public int mul(int n1, int n2) {
+        return n1 * n2;
+    }
+
+    public int div(int n1, int n2) {
+        return n1 / n2;
+    }
+}
---
+++ 11_inheritance/Inheritance_01.java
@@ -0,0 +1,14 @@
+
+public class Inheritance_01 {
+    public static void main(String[] args) {
+        // Calc_02 obj = new Calc_02(); // you can acces the other file using object it is inheritance
+
+        // int r1 = obj.add(4,5);
+        // int r2 = obj.add(7,3);
+        // int r3 = obj.mul(4,5);
+        // int r4 = obj.div(7,3);
+
+        // System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+
+    }
+}
