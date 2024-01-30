e203b84f7dd963b7d1c3071173ce82a7eb899065
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-01-30 13:19:24 +0000

    Naming Canvention

---
+++ 10_Other_cansept/Naming_Canvention_02.java
@@ -0,0 +1,16 @@
+public class Naming_Canvention_02 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// Camel casting (for readebility)
+
+// class and interface - Calc, Runable
+// variable and method - marks, show()
+// constants - PIE, BRAND
+
+// showMyMarks()
+// MyData
+
+// age, DATA, Human()
