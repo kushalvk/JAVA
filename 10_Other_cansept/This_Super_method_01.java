5b33d59f99277453da903d0ea6d611884d4e33ad
kushal <kushalvaghela2003@gmail.com>
2024-04-08 12:59:42 +0530

    This and Super Keyword

--- 10_Other_cansept/This_Super_method_01.java
+++ 10_Other_cansept/This_Super_method_01.java
@@ -1,24 +1,24 @@
-class A extends Object { // this extends "Object" is by default in every base class.
-    public A() {
+class A1 extends Object { // this extends "Object" is by default in every base class.
+    public A1() {
         super();
-        System.out.println("in A");
+        System.out.println("in A1");
     }
-    public A(int n) {
+    public A1(int n) {
         super();
-        System.out.println("in A int");
+        System.out.println("in A1 int");
     }
 }
 
-class B extends A{
-    public B() {
+class B1 extends A1{
+    public B1() {
         super(); // if we pasing a paramitar the super class is called paramitaraic canstractor
-        System.out.println("in B");
+        System.out.println("in B1");
     }
-    public B(int n) {
+    public B1(int n) {
         // super(n); // int this select the paramitarice canstractar.
         // if we not defaind a paramitar(n) the it select a default canstractore
         this(); // "this" will execute the canstractor of a same class
-        System.out.println("in B int");
+        System.out.println("in B1 int");
     }
 }
 
@@ -26,8 +26,8 @@ class B extends A{
 
 public class This_Super_method_01 {
     public static void main(String[] args) {
-        B obj = new B(5);
-        // B obj = new B();
+        B1 obj = new B1(5);
+//         B1 obj = new B1();
 
     }
 }
