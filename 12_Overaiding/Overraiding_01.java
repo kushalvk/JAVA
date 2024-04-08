9789bbe36f52913a1d5e6f543b98c22e246eb78b
kushal <kushalvaghela2003@gmail.com>
2024-04-08 12:58:14 +0530

    Overraiding

--- 12_Overaiding/Overraiding_01.java
+++ 12_Overaiding/Overraiding_01.java
@@ -1,9 +1,9 @@
-class A {
+class A2 {
     public int show(int n1,int n2) {
         return n1 + n2;
     }
 }
-class B extends A {
+class B2 extends A2 {
     public int show(int n1,int n2) { // this is method overraiding
         return n1 + n2 + 1;
     }
@@ -11,7 +11,7 @@ class B extends A {
 
 public class Overraiding_01 {
     public static void main(String[] args) {
-        B obj = new B();
+        B2 obj = new B2();
         obj.show(4, 3);
     }
 }
