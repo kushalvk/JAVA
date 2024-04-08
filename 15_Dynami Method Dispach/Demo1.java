f03912da0d025d581c5466205973e88c3b869026
kushal <kushalvaghela2003@gmail.com>
2024-04-08 12:56:43 +0530

    Dynami Method Dispach

--- 15_Dynami Method Dispach/Demo1.java
+++ 15_Dynami Method Dispach/Demo1.java
@@ -1,30 +1,30 @@
 // Dynamic methid dispach
 
-class A {
+class A3 {
     public void show() {
         System.out.println("in A show");
     }
 }
-class B extends A {
+class B3 extends A3 {
     public void show() {
         System.out.println("in B show");
     }
 }
-class C extends A {
+class C3 extends A3 {
     public void show() {
         System.out.println("in C show");
     }
 }
 
-public class Demo {
+public class Demo1 {
     public static void main(String[] args) {
-        A obj = new A();
+        A3 obj = new A3();
         obj.show(); // in A show
 
-        obj = new B();
+        obj = new B3();
         obj.show(); // in B show
 
-        obj = new C();
+        obj = new C3();
         obj.show(); // in C show
     }
 }
