a016c0d01def21da58a216587926d56d23fa017c
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-04-09 04:13:52 +0000

    Inner class

---
+++ 21_inner class/Inner_class.java
@@ -0,0 +1,31 @@
+
+
+class A6 {
+    int age;
+
+    public void show2() {
+        System.out.println("in show");
+    }
+
+    class B6 {
+        public void config() {
+            System.out.println("in config");
+        }
+    }
+
+    static class C6 {
+        
+    }
+}
+
+public class Inner_class {
+    public static void main(String[] args) {
+        A6 obj = new A6();
+        obj.show2();
+
+        A6.B6 obj1 = obj.new B6();
+        obj1.config();
+
+        A6.C6 obj2 = new A6.C6(); // if inner class is static
+    }
+}
