6f91a60d6a6f0b1f7c798122b1298392dbc95912
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-04-09 12:43:20 +0000

    Intarface

--- 22_Intarface/Intarface_02.java
+++ 22_Intarface/Intarface_02.java
@@ -1,3 +1,7 @@
+// class - class -> extends
+// class - intarface -> implements
+// intarface - intarface -> extends
+
 interface A8 {
     int age = 21;    // all the variable are final and static
     String area = "Surat";
@@ -6,12 +10,24 @@ interface A8 {
     void config4();
 }
 
-class B8 implements A8 { // inplement Intarface - all the method are declared in intarface are applay in class campalsary
+interface x {
+    void run();
+}
+
+interface y extends x {
+    
+}
+
+class B8 implements A8,y { // inplement Intarface - all the method are declared in intarface are applay in class campalsary
     public void show4(){
         System.out.println("in show");
     }
     public void config4() {
         System.out.println("in config");
+    }
+
+    public void run() {
+        System.out.println("runing...");
     }
 }
 
@@ -20,6 +36,10 @@ public class Intarface_02 {
         A8 obj = new B8();
         obj.show4();
         obj.config4();
+        // obj.run();   // we can not access run method direcly b/s A8 don't know what is 'run' method
+
+        x obj1 = new B8();
+        obj1.run();
 
         // A8.age = 22; // you can't change the value of age b/s it's 'final'
 
