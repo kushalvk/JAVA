9295709788f427d23ce6c2b9a21b42537a888622
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-04-10 04:26:52 +0000

    Type Intarface

---
+++ 22_Intarface/Type_Intarface.java
@@ -0,0 +1,23 @@
+public class Type_Intarface {
+    public static void main(String[] args) {
+        // types of Intarface
+        //      1) Normal
+        //      2) Funcational/SAM
+        //      3) Marker
+
+        //  1) Noramal :-
+        // interface abc {
+        //     two or more methods
+        // }
+
+        //  2) Function/SAM :- (Single Abstract Method)
+        // interface abc {
+        //     Only one Method
+        // }
+
+        //  3)  Marker
+        // interface abc {
+        //     No Method (Blank Intarface)
+        // }
+    }
+}
