79c133088bb84403a98cee2843b5ab9a0eb6b66a
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-04-11 14:26:27 +0000

    Runnable intaface

--- 27_Threads/Thread_01.java
+++ 27_Threads/Thread_01.java
@@ -1,7 +1,8 @@
 //  Thread is use to handaled maltipal task at a particular point of time.
+
 class A14 extends Thread {
     public void run() {
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 0; i <= 5; i++) {
             System.out.println("hi");
             try {
                 Thread.sleep(1000);
@@ -12,9 +13,10 @@ class A14 extends Thread {
     }
 }
 
-class B14 extends Thread {
+class B14 implements Runnable { // you can also create a Thread also by Runnebale Method.
+    // you can use the Lambda Expretion( = () -> {}; ) here b/s Runnable intarface is @FunctionalInterface type Intaface
     public void run() {
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 0; i <= 5; i++) {
             System.out.println("hello");
             try {
                 Thread.sleep(1000);
@@ -41,6 +43,11 @@ public class Thread_01 {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        obj2.start(); // both the Thred are work a aeme time
+
+        // you cannot access the start method in runnable intarface.(you want to
+        // implement following)
+        Thread t1 = new Thread(obj2);
+        t1.start();
+        // both the Thred are work a aeme time
     }
 }
