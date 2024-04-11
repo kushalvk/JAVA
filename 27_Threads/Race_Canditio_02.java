06e80876a4a4be94163c316dd21132a136b22a67
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-04-11 14:53:47 +0000

    Synchronized Thread

---
+++ 27_Threads/Race_Canditio_02.java
@@ -0,0 +1,37 @@
+
+class Counter {
+    int count;
+    public synchronized void increment() { // 'synchronized' is use to execute a one Thread at a time the other Thread is waiting.
+        count++;
+    }
+}
+
+public class Race_Canditio_02 {
+    public static void main(String[] args) throws InterruptedException {
+        Counter cn = new Counter();
+
+        // Lembda Expretion " = () -> {}; "
+        Runnable obj = () -> {
+            for (int i = 1; i < 1000; i++) {
+                cn.increment();
+            }
+        };
+
+        Runnable obj1 = () -> {
+            for (int i = 1; i < 1000; i++) {
+                cn.increment();
+            }
+        };
+
+        Thread t1 = new Thread(obj);
+        Thread t2 = new Thread(obj1);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(cn.count);
+    }
+}
